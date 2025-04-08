2a477fceca9759adb6f7e15a855a93cea438077a
Brian LT <brian.lund.t@outlook.com>
2025-04-08 12:55:04 -0400

    Se agregan Imagenes, para construccion-embarcaciones.html, se agregan estilos para esta , se agrega funcion lightbox para poder agrandar estas imagenes con un js ´para esto

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("nav-principal").classList.toggle("show");
   });
   
+// LIGHTBOX sencillo
+document.addEventListener('DOMContentLoaded', () => {
+  const overlay = document.createElement('div');
+  overlay.className = 'lightbox-overlay';
+  document.body.appendChild(overlay);
+
+  const img = document.createElement('img');
+  overlay.appendChild(img);
+
+  document.querySelectorAll('.lightbox-trigger').forEach(image => {
+    image.addEventListener('click', () => {
+      img.src = image.src;
+      overlay.style.display = 'flex';
+    });
+  });
+
+  overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
+});
